5a7987d08806c9b1bc5cfe43851d48d1ddd5ab87
Vy <ks031088@gmail.com>
2020-07-20 22:11:35 +0500

    Handler was wrapped into a function

--- public/js/product_card_click_handler.js
+++ public/js/product_card_click_handler.js
@@ -1,42 +1,43 @@
-nodes = document.querySelectorAll("div.product-card");
-//every single card
-Array.from(nodes).forEach((node) => {
-    let cart = [];
-    //console.log(node);
-    let nodeW = node.querySelectorAll("div.weights div.weights_item");
-    let btnAdd = node.querySelector("div.button-add");
-    let btnAddText = btnAdd.querySelector('div.btn_add_text');
-    let nodeW_price = btnAdd.querySelector("div.tp-price");
+function main() {
+
+    let nodes = document.querySelectorAll("div.product-card");
+    //every single card
+    Array.from(nodes).forEach((node) => {
+        let cart = [];
+        //console.log(node);
+        let nodeW = node.querySelectorAll("div.weights div.weights_item");
+        let btnAdd = node.querySelector("div.button-add");
+        let btnAddText = btnAdd.querySelector('div.btn_add_text');
+        let nodeW_price = btnAdd.querySelector("div.tp-price");
 
 
-    Array.from(nodeW).forEach((elt) => {
-        //click to a weight
-        elt.addEventListener("click", e => {
-            let price = elt.getAttribute("data-price");
-            if (price) {
-                nodeW_price.textContent = price;
-            } else {
-                nodeW_price.textContent = 'unkonwn';
-            }
-            delClassElt(nodeW, 'selected');
-            elt.classList.add('selected');
+        Array.from(nodeW).forEach((elt) => {
+            //click to a weight
+            elt.addEventListener("click", e => {
+                let price = elt.getAttribute("data-price");
+                if (price) {
+                    nodeW_price.textContent = price;
+                } else {
+                    nodeW_price.textContent = 'unkonwn';
+                }
+                delClassElt(nodeW, 'selected');
+                elt.classList.add('selected');
 
-            if (btnAddText.classList.contains('selected')) {
-                btnAddText.classList.remove('selected')
-                btnAddText.textContent = 'В корзину';
-            }
+                if (btnAddText.classList.contains('selected')) {
+                    btnAddText.classList.remove('selected')
+                    btnAddText.textContent = 'В корзину';
+                }
+            })
         })
+
+        //btnAdd handler
+        btnAdd.addEventListener("click", e => {
+            btnAddText.classList.add('selected');
+            btnAddText.textContent = 'Выбрано'
+        });
     })
 
-    //btnAdd handler
-    btnAdd.addEventListener("click", e => {
-        btnAddText.classList.add('selected');
-        btnAddText.textContent = 'Выбрано'
-    });
-})
-
-
-
+}
 
 function showCart(cart) {
     console.log(cart)
@@ -49,3 +50,5 @@ function delClassElt(nodeList, classElt) {
         }
     })
 }
+
+main();
