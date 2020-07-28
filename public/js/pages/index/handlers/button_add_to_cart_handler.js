73d3a0c38d721327919d7e9fc312513b8048fb56
Vy <ks031088@gmail.com>
2020-07-28 15:59:48 +0500

    Changed structure of JavaScript files for web pages

---
+++ public/js/pages/index/handlers/button_add_to_cart_handler.js
@@ -0,0 +1,21 @@
+function buttonAddProductToCartHandler(button, weightButtons, productName, selectedProducts, st) {
+    text = button.querySelector('div.btn_add_text');
+    button.addEventListener("click", function(e) {
+        weightButtons.forEach(function(elt) {
+            if (isSelected(elt, st)) {
+                text.classList.add(st);
+                text.textContent = 'В корзине';
+                selectedProducts.push({
+                    "name": productName,
+                    'weight': elt.innerText,
+                    "price": elt.getAttribute('data-price'),
+                });
+            }
+        });
+        console.log('\n');
+        selectedProducts.forEach(function(e) {
+            console.log(e['name'] + " " + e['weight'] + " " + e['price']);
+        });
+
+    });
+}
---
+++ public/js/pages/index/handlers/button_weight_handler.js
@@ -0,0 +1,27 @@
+function buttonWeightHandler(weightButtons,
+    priceForWeight,
+    node_weights,
+    buttonAddValue, st) {
+
+    weightButtons.forEach((elt) => {
+        //click to a weight
+        if (!isSelected(elt, st)) {
+            elt.addEventListener("click", e => {
+                let price = elt.getAttribute("data-price");
+                if (price) {
+                    priceForWeight.textContent = price;
+                } else {
+                    priceForWeight.textContent = 'unkonwn';
+                }
+                removeClassNameFromNodeList(node_weights, st);
+                elt.classList.add(st);
+
+                if (buttonAddValue.classList.contains(st)) {
+                    buttonAddValue.classList.remove(st)
+                    buttonAddValue.textContent = 'Выбрать';
+                }
+
+            });
+        }
+    });
+}
