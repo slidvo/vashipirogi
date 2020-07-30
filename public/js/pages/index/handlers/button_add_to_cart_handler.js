66184f56f657676513d7be9c5b89c22e9a15dd8b
Vy <ks031088@gmail.com>
2020-07-30 20:39:38 +0500

    Made remove button and clear button from basket

--- public/js/pages/index/handlers/button_add_to_cart_handler.js
+++ public/js/pages/index/handlers/button_add_to_cart_handler.js
@@ -24,39 +24,3 @@ function buttonAddProductToCartHandler(productList, totalPriceItem, button,
 
     });
 }
-
-
-function basketFilling(pl, tPi, sp) { //sp.id name weight price
-    let productListItem = document.createElement('div');
-    productListItem.classList.add('product_list__item');
-    let elt_remove = document.createElement("div");
-    elt_remove.classList.add('elt_remove');
-    let img = document.createElement('img');
-    img.src = "images/quit.png";
-    elt_remove.appendChild(img);
-
-    productListItem.appendChild(elt_remove);
-
-    let elt_name = document.createElement('div');
-    let elt_weight = document.createElement('div');
-    let elt_price = document.createElement('div');
-
-    elt_name.classList.add('elt_name');
-    elt_weight.classList.add('elt_weight');
-    elt_price.classList.add('elt_price');
-
-    elt_name.appendChild(document.createTextNode(sp.name));
-    elt_weight.appendChild(document.createTextNode(sp.weight));
-    elt_price.appendChild(document.createTextNode(sp.price));
-
-    productListItem.appendChild(elt_name);
-    productListItem.appendChild(elt_weight);
-    productListItem.appendChild(elt_price);
-
-    pl.appendChild(productListItem);
-    console.log(sp);
-
-    let result = Number(sp.price) + Number(tPi.textContent);
-    tPi.textContent = result;
-
-}
--- public/js/pages/index/handlers/button_basket_handler.js
+++ public/js/pages/index/handlers/button_basket_handler.js
@@ -1,19 +1,30 @@
 // page-4 handler
-function bascketHandler(st) {
+function bascketHandler(pl, dc, st) {
     let btn = document.querySelector('div.button_basket');
     let p4 = document.querySelector('.page-4');
-    let b1 = document.querySelector('button.b1');
-    let b2 = document.querySelector('button.b2');
+    let b1 = p4.querySelector('button.b1');
+    let b2 = p4.querySelector('button.b2');
 
-    btn.addEventListener('click', function(e) {
-        p4.classList.remove('close');
-        p4.classList.add('show');
+    btn.addEventListener('click', function() {
+        if (pl.firstChild) {
+            p4.classList.remove('close');
+            p4.classList.add('show');
 
+            let totalPrice = p4.querySelector('div.total_price__item');
+            totalPrice.textContent = Number(totalPrice.textContent) + dc;
+
+        } else {
+            alert("Корзина пустая :(")
+        }
     });
 
     b1.addEventListener('click', function() {
         p4.classList.remove('show');
         p4.classList.add('close');
         btn.classList.remove(st);
+    })
+
+    b2.addEventListener('click', function() {
+        removeChildren(pl);
     })
 }
