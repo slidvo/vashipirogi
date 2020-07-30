f7a3632cedc4c233f20d7e2af482a265e158b339
Vy <ks031088@gmail.com>
2020-07-31 00:32:13 +0500

    Added page for register order

--- public/js/pages/index/handlers/button_basket_handler.js
+++ public/js/pages/index/handlers/button_basket_handler.js
@@ -4,11 +4,11 @@ function bascketHandler(pl, dc, st) {
     let p4 = document.querySelector('.page-4');
     let b1 = p4.querySelector('button.b1');
     let b2 = p4.querySelector('button.b2');
+    let b3 = p4.querySelector('button.b3');
 
-    btn.addEventListener('click', function() {
+    btn.addEventListener('click', function(e) {
         if (pl.firstChild) {
-            p4.classList.remove('close');
-            p4.classList.add('show');
+            p4.style.display = 'grid';
 
             let totalPrice = p4.querySelector('div.total_price__item');
             totalPrice.textContent = Number(totalPrice.textContent) + dc;
@@ -18,13 +18,41 @@ function bascketHandler(pl, dc, st) {
         }
     });
 
-    b1.addEventListener('click', function() {
-        p4.classList.remove('show');
-        p4.classList.add('close');
+    b1.addEventListener('click', function(e) {
+        p4.style.display = 'none';
         btn.classList.remove(st);
     })
 
-    b2.addEventListener('click', function() {
+    b2.addEventListener('click', function(e) {
         removeChildren(pl);
+    })
+
+    b3.addEventListener('click', function(e) {
+        let registerOrderPage = document.getElementById("register_order");
+        if (pl.firstChild) {
+            p4.style.display = 'none';
+            registerOrderPage.style.display = "grid";
+            let registerOrderProductList = document.querySelector("div.register_order__info div.product_list");
+            let plItems = pl.querySelectorAll("div.product_list__item");
+
+            Array.from(plItems).forEach(function(elt) {
+                let id = elt.getAttribute('data-id');
+                let weight = elt.getAttribute('data-weight');
+                let name = elt.querySelector("div.elt_name");
+                let eWeight = elt.querySelector("div.elt_weight");
+                let cost = elt.querySelector("div.elt_price");
+                let roProductListItem = document.createElement('div');
+                roProductListItem.classList.add('product_list__item');
+
+                roProductListItem.appendChild(name);
+                roProductListItem.appendChild(eWeight);
+                roProductListItem.appendChild(cost);
+                roProductListItem.setAttribute('data-id', id);
+                roProductListItem.setAttribute('data-weight', weight);
+                registerOrderProductList.appendChild(roProductListItem);
+            });
+
+
+        }
     })
 }
