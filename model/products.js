08e933e1027d265bf1ac13e1f237054de56f7661
Vy <ks031088@gmail.com>
2020-08-07 14:56:15 +0500

    Returned few deleted proucts prior

--- model/products.js
+++ model/products.js
@@ -45,13 +45,13 @@ let cakes = [
     new Cake(6, 0, "Пирог  \"День и ночь\"", "мак, творог", "images/cakes/day_and_night.jpg"),
     new Cake(7, 0, "Пирог  \"Нежность\"", "творог, ванилин", "images/cakes/fondness.jpg"),
     new Cake(8, 0, "Пирог  \"Маковый\"", "мак, брусника", "images/cakes/poppy.jpg"),
-    // new Cake(9, 0, "Пирог с черной смородиной", "черная смородина, взбитые сливки/сметана", "images/cakes/blackberry.jpg"),
-    // new Cake(10, 0, "Пирог c черникой", "черника, взбитые сливки/сметана", "images/cakes/blueberry.jpg"),
+    new Cake(9, 0, "Пирог с черной смородиной", "черная смородина, взбитые сливки/сметана", "images/cakes/blackberry.jpg"),
+    new Cake(10, 0, "Пирог c черникой", "черника, взбитые сливки/сметана", "images/cakes/blueberry.jpg"),
     //Vegetables
     new Cake(11, 1, "Пирог с капустой", "капуста", "images/cakes/cabbage.jpg"),
     new Cake(12, 1, "Пирог с капустой и яйцом", "капуста, яйцо", "images/cakes/cabbage_egg.jpg"),
     new Cake(13, 1, "Пирог с капустой и грибами", "капуста, грибы", "images/cakes/cabbage_mushrooms.jpg"),
-    // new Cake(14, 1, "Пирог с картофелем", "картофель, лук, зелень", "images/cakes/potato.jpg"),
+    new Cake(14, 1, "Пирог с картофелем", "картофель, лук, зелень", "images/cakes/potato.jpg"),
 
     //hearty
     new Cake(15, 2, "Пирог с сыром", "сыр, зелень", "images/cakes/chees.jpg"),
@@ -66,8 +66,8 @@ let cakes = [
     new Cake(23, 2, "Пирог с горбушей и картофелем", "горбуша, картофель, лук, зелень", "images/cakes/pink_salmon_potato.jpg"),
     new Cake(24, 2, "Пирог с судаком и картофелем", "судак, картофель, лук, зелень", "images/cakes/zander_potato.jpg"),
     new Cake(25, 2, "Пирог с индейкой и рисом с грибами", "филе индейки, рис, грибы, лук, зелень", "images/cakes/turkey_rice_mushrooms.jpg"),
-    // new Cake(26, 2, "Пирог с горбушей и рисом", "горбуша, картофель/рис, лук, зелень", "images/cakes/pink_salmon_rice.jpg"),
-    // new Cake(27, 2, "Пирог с судаком и рисом", "судак, рис, лук, зелень", "images/cakes/zander_rice.jpg"),
+    new Cake(26, 2, "Пирог с горбушей и рисом", "горбуша, картофель/рис, лук, зелень", "images/cakes/pink_salmon_rice.jpg"),
+    new Cake(27, 2, "Пирог с судаком и рисом", "судак, рис, лук, зелень", "images/cakes/zander_rice.jpg"),
 
 
 
